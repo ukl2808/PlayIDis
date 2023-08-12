61ae59551098f830301e0e39c030e4126ec738b4
Admin <Admin@DESKTOP-047GBOQ>
2023-08-12 12:16:03 +0300

    Добавьте файлы проекта.

---
+++ PlayIDis/Play.cs
@@ -0,0 +1,44 @@
+﻿class Play : IDisposable
+{
+    private string title;
+    private string author;
+    private string genre;
+    private int releaseYear;
+
+    public Play(string title, string author, string genre, int releaseYear)
+    {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+
+    public string Genre
+    {
+        get { return genre; }
+        set { genre = value; }
+    }
+
+    public int ReleaseYear
+    {
+        get { return releaseYear; }
+        set { releaseYear = value; }
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"Resource cleanup: Play \"{Title}\".");
+    }
+}
---
+++ PlayIDis/Program.cs
@@ -0,0 +1,10 @@
+﻿using (Play play = new Play("Hamlet", "William Shakespeare", "Tragedy", 1600))
+{
+    Console.WriteLine("Play Information:");
+    Console.WriteLine($"Title: {play.Title}");
+    Console.WriteLine($"Author: {play.Author}");
+    Console.WriteLine($"Genre: {play.Genre}");
+    Console.WriteLine($"Release Year: {play.ReleaseYear}");
+}
+
+Console.WriteLine("Program completed.");
